3f26575b592a874296aee9ef2fd54623758b9d23
sa1dakhmedovv <159274703+sa1dakhmedovv@users.noreply.github.com>
2024-07-21 22:50:19 +0500

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,17 +1,28 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from users import views as myapp_views
+"""config URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', myapp_views.home, name="home"),
-    path('register', myapp_views.regiter, name='register'),
-    path('login', myapp_views.login_view, name='login'),
-    path('logout', myapp_views.logout_view, name='logout'),
-    path('profile', myapp_views.profile, name='profile'),
-
-    path('password-reset', myapp_views.CustomPasswordResetView, name='password_reset'),
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete')
+    path('admin/', admin.site.urls),
+    path('',include('users.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
